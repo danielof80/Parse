0af8656af3fe1f9577b017b1ef1a1404c63334b2
Daniel <Daniel@Daniel-PC>
2013-06-03 09:38:22 +0100

    proyect tree module tree

---
+++ src/tree/Tree.java
@@ -0,0 +1,7 @@
+package tree;
+
+public abstract class Tree {
+    public abstract Type evaluate();
+    @Override
+    public abstract String toString();
+}
---
+++ src/tree/Type.java
@@ -0,0 +1,14 @@
+package tree;
+
+public class Type<T> {
+
+    private T value;
+
+    public Type(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
