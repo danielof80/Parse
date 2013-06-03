4e2d31ba9d86747567855f2e0c722757f7280f20
Daniel <Daniel@Daniel-PC>
2013-06-03 10:18:19 +0100

    module operators

---
+++ src/calculator/operators/OperatorBinary.java
@@ -0,0 +1,13 @@
+package calculator.operators;
+import tree.Operator;
+
+public class OperatorBinary extends Operator {
+    public static final OperatorBinary ADD = new OperatorBinary("+", "add", 1);
+    public static final OperatorBinary SUBS = new OperatorBinary("-", "substract", 1);
+    public static final OperatorBinary MUL = new OperatorBinary("*", "mul", 2);
+    public static final OperatorBinary DIV = new OperatorBinary("/", "div", 2);
+    
+    private OperatorBinary(String info, String name, int order) {
+        super(info, name, order);
+    }
+}
