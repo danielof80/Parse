497246b6c710d31a05f3231c892a2d497c20b4e7
Daniel <Daniel@Daniel-PC>
2013-06-03 09:56:02 +0100

    module tree variable,operator

---
+++ src/tree/Variables.java
@@ -0,0 +1,33 @@
+package tree; 
+
+class Variables extends Tree {
+
+    private Type type;
+
+    public Variables(Type value) {
+        this.type = value;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+    
+    public Object getValue(){
+        return type.getValue();
+    }
+
+    @Override
+    public Type evaluate() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
